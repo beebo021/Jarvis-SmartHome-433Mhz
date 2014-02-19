dbb54ea4bd67ea73db4dc621c2ed5d746ded2660
Alejandro Lopez <w@alpz.es>
2014-02-19 14:16:39 +0000

    Removes script start.sh, included now in to init.d/jarvis

--- www/api/v1/Daemons/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /var/www/jarvis/www/api/v1/;
-screen -DmS jarvis1 php Daemons/daemonCmdsQueue.php&
-screen -DmS jarvis2 php Daemons/daemonRulesTimely.php&
-screen -DmS jarvis3 python Daemons/daemonReceiveRF433.py&
